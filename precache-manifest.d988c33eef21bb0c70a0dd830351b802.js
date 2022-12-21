36c0345c23fddf2c6d28acf06c35b0402e187996
Anil Kumar <45123631+remoteservice1979@users.noreply.github.com>
2022-12-21 18:00:13 +0530

    Add files via upload
    Base file

---
+++ precache-manifest.d988c33eef21bb0c70a0dd830351b802.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15531f44e681c519fa70d9e6c445b5bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "720b62dc92c383202643",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ecece2a068338766af3d",
+    "url": "/static/js/2.2a8ec5fb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2a8ec5fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "720b62dc92c383202643",
+    "url": "/static/js/main.30f1effb.chunk.js"
+  },
+  {
+    "revision": "bbcec7f2cb23bcc69ca1",
+    "url": "/static/js/runtime-main.8c140d52.js"
+  }
+]);
